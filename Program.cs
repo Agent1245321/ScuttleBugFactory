c366fa524c9cb6e0cf6a435a7239ad6e508b53f8
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-08-14 11:00:17 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpeedRun;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+int scuttleBugCount = 0;
+double legs;
+int eyes;
+string name;
+bool anotherOne;
+string answer;
+
+//A method having the user make a scuttlebug
+void MakeScuttlebug()
+{
+    //asks the user for this cuttlebugs leg count and sets its legs to that
+    Console.WriteLine($"How many legs should your child have?");
+    legs = Convert.ToDouble(Console.ReadLine());
+
+    //asks the user for this cuttlebugs eye count and sets its eyes to that
+    Console.WriteLine($"How many eyes?");
+    eyes = Convert.ToInt32(Console.ReadLine());
+
+    //asks the user for this cuttlebugs name and saves it
+    Console.WriteLine($"What shall you name this child?");
+    name = Console.ReadLine();
+
+    //adds to the count
+    scuttleBugCount ++;
+
+    //instaniates a new scuttlebug and uses all the variables taken from the user as the arguments
+    ScuttleBug emptyVessel = new ScuttleBug(legs, eyes, name, scuttleBugCount);
+
+    //asks the user if they wish to make another scuttlebug
+    Console.WriteLine("Do You Wish To Create another child?");
+    answer = Console.ReadLine();
+
+
+    //ches if the answer is yes and calls the method again
+    if(answer == "y" || answer == "yes" || answer == "Y" || answer == "Yes")
+        {
+        MakeScuttlebug();
+    }
+    //tells the user goodbye if the answer is not yes
+    else
+    {
+        Console.WriteLine("enjoy your death");
+    }
+}
+
+
+
+//Makes a scuttlebug
+MakeScuttlebug();
+
---
+++ ScuttleBug.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRun
+{
+    internal class ScuttleBug
+    {
+        //creating the base variables for the scuttleBug objects;
+        double legs;
+        int eyes;
+        int id;
+        string name;
+
+
+        //creating the properties for the variables, this allows us to restrict the access to them in other classes;
+        public double Legs { get; private set; }
+        public int Eyes { get; private set; }
+        
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+
+        //this is the ScuttleBug Object, it takes 4 arguments to define the bug;
+        public ScuttleBug(double legs, int eyes, string name, int iD)
+        {
+            // sets the properties of this scuttlebug
+            this.Legs = legs;
+            this.Eyes = eyes;
+            this.Name = name;
+            this.ID = iD;
+
+            //tells the console about the scuttleBug
+            Console.WriteLine($"This Scuttlebug has {this.Legs} legs and {this.Eyes} eyes, his name is {name}.");
+            Console.WriteLine($"He is the {Generation(iD)} of his kind");
+        }
+
+
+        //switch statement to handle generation string
+        string Generation(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return "1st";
+                    break;
+
+                case 2:
+                    return "2nd";
+                    break;
+
+                case 3:
+                    return "3rd";
+                    break;
+
+                case 4:
+                    return "4th";
+                    break;
+
+                case 5:
+                    return "5th";
+                    break;
+
+                case 6:
+                    return "6th";
+                    break;
+
+                case 7:
+                    return "7th";
+                    break;
+
+                case 8:
+                    return "8th";
+                    break;
+
+                case 9:
+                    return "9th";
+                    break;
+
+                case 10:
+                    return "10th";
+                    break;
+
+                default:
+                    return "god";
+                    break;
+            }
+
+        }
+    }
+}
